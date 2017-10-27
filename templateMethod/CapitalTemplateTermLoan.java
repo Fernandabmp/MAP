721fe52202116553a6f320d33396ec35355edbb8
Fernanda Mendonça <fer.nanda.bdmp@gmail.com>
2017-10-27 08:54:36 -0300

    Update CapitalTemplateTermLoan.java

--- templateMethod/CapitalTemplateTermLoan.java
+++ templateMethod/CapitalTemplateTermLoan.java
@@ -3,6 +3,6 @@ package br.padrao.templateMethod;
 public class CapitalTemplateTermLoan extends CapitalTemplate{
 
 	public double capital(Loan loan) {
-		return(loan.getCommitment() * duration(loan) * riskFactorFor(loan));				
+		return(loan.getCommitment() * capitalTemplate(loan));				
 	}
 }
