522098a7c0d8adc4e807381ac14ef0710666dfa6
Fernanda Mendonça <fer.nanda.bdmp@gmail.com>
2017-10-27 08:55:42 -0300

    Exercício 09

--- templateMethod/CapitalTemplateTermLoan.java
+++ templateMethod/CapitalTemplateTermLoan.java
@@ -1,6 +1,6 @@
 package br.padrao.templateMethod;
 
-public class CapitalTemplateTermLoan extends CapitalTemplate{
+public class CapitalTemplateTermLoan extends CapitalTemplate {
 
 	public double capital(Loan loan) {
 		return(loan.getCommitment() * capitalTemplate(loan));				
