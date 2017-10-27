cc29ce5b282784a287e98614c4974cfef5e3ce77
Fernanda Mendonça <fer.nanda.bdmp@gmail.com>
2017-10-27 08:56:19 -0300

    Exercício 09

--- templateMethod/CapitalTemplateAdvisedLine.java
+++ templateMethod/CapitalTemplateAdvisedLine.java
@@ -1,8 +1,8 @@
 package br.padrao.templateMethod;
 
-public class CapitalTemplateAdvisedLine extends CapitalTemplate{
+public class CapitalTemplateAdvisedLine extends CapitalTemplate {
 
 	public double capital(Loan loan) { 
-		return(loan.getCommitment() * loan.getunusedPercentage() * duration(loan) * riskFactorFor(loan));
+		return(loan.getCommitment() * loan.getunusedPercentage() * capitalTemplate(loan));
 	}	
 }
